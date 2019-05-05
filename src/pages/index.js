b6d499d4b86398df64c62971215d114753642fc0
harrygreen <harryharryharry@gmail.com>
2019-05-05 19:58:53 +0100

    added 2nd rotate

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,31 +13,31 @@ const IndexPage = () => (
     <div className="text-white flex items-center justify-center flex-col min-h-screen">
       <div className="sticks w-full">
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate" />
+          <span className="stick animated infinite rotate1" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate" />
+          <span className="stick animated infinite rotate2" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate reverse" />
+          <span className="stick animated infinite rotate1 reverse" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate" />
+          <span className="stick animated infinite rotate1" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate reverse" />
+          <span className="stick animated infinite rotate2 reverse" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate" />
+          <span className="stick animated infinite rotate1" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate reverse" />
+          <span className="stick animated infinite rotate1 reverse" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate" />
+          <span className="stick animated infinite rotate1" />
         ))}
         {[1, 2, 3, 4, 5].map(i => (
-          <span className="stick animated infinite rotate reverse" />
+          <span className="stick animated infinite rotate1 reverse" />
         ))}
       </div>
     </div>
