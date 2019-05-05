0f6f2f552b4376cf7727cfc811be98293eff32af
harrygreen <harryharryharry@gmail.com>
2019-05-05 16:24:56 +0100

    moved spin to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,65 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import "./spin.css"
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    {/* <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} /> */}
+    <div className="text-white flex items-center justify-center flex-col">
+      <div className="">
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate reverse" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate reverse" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate reverse" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate reverse" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate" />
+          ))}
+        </div>
+        <div className="flex">
+          {[1, 2, 3, 4, 5].map(i => (
+            <span className="stick animated infinite rotate reverse" />
+          ))}
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
